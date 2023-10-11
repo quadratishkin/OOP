1390c5e62ed85b59ea2e8a4ebd8a54c88c89ff52
Nikita Volkov <n.volkov1@g.nsu.ru>
2023-10-11 19:21:40 +0700

    Folder changed

--- Task1_1_2/myPolynom/src/main/sth.bat
+++
@@ -1,5 +0,0 @@
-javac -d .\out .\java\org\example\Main.java .\java\org\example\Polynomial.java
-javadoc -d .\javadoc .\java\org\example\Main.java .\java\org\example\Polynomial.java
-cd .\out
-java -classpath . org/example.Main
-pause
